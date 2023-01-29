dd1ea7ae912a972dffe237dd3b335bec83f8c1c6
Jarosław Klimas <admin@javappa.com>
2023-01-30 00:59:12 +0100

    JVPA-9 Java records presented

---
+++ java-records/src/Start.java
@@ -0,0 +1,33 @@
+public class Start {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Item item1 = new Item(1L, "Your first item", "desc1");
+        Item item2 = new Item(1L, "Your first item", "desc2");
+
+        System.out.println(item1);
+        System.out.println(item2);
+
+        System.out.println(item1.equals(item2));
+
+        System.out.println("======================");
+
+        BestItem bestItem1 = new BestItem(1L, "your best item no 1", "desc1");
+        BestItem bestItem2 = new BestItem(1L, "your best item no 1", "desc1");
+
+        System.out.println(bestItem1);
+        System.out.println(bestItem2);
+
+        System.out.println(bestItem1.equals(bestItem2));
+
+        System.out.println("======================");
+
+        System.out.println(item1.getName());
+        System.out.println(bestItem1.name());
+
+        System.out.println(item1.getId());
+        System.out.println(bestItem1.id());
+
+        bestItem1.someMethod();
+    }
+}
