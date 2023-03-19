23cf81d7db6e2ba5c4c8e26530664b47229a3339
Jarosław Klimas <admin@javappa.com>
2023-03-19 18:04:27 +0100

    JVPA-19 Handling API responses

--- spring-boot-3-web/src/main/java/com/javappa/start/service/SportEventService.java
+++ spring-boot-3-web/src/main/java/com/javappa/start/service/SportEventService.java
@@ -1,16 +1,19 @@
 package com.javappa.start.service;
 
+import com.javappa.start.api.NewSportEventRequest;
 import com.javappa.start.api.SportEventResponse;
+import com.javappa.start.api.UpdateSportEventRequest;
 import com.javappa.start.domain.SportEvent;
-import io.swagger.v3.oas.annotations.Operation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 
+@Slf4j
 @Service
 public class SportEventService {
 
@@ -22,11 +25,39 @@ public class SportEventService {
         events.put(2L, new SportEvent(2L, "Football match"));
         events.put(3L, new SportEvent(3L, "Boxing fight"));
     }
-
     public List<SportEventResponse> getAll() {
         return events.values().stream()
                 .map(sportEvent -> new SportEventResponse(sportEvent.getId(),
                         sportEvent.getName()))
                 .toList();
+    }
+
+    public Long create(NewSportEventRequest eventRequest) {
+        SportEvent event = new SportEvent(index.incrementAndGet(), eventRequest.name());
+        events.put(index.get(), event);
+        log.info("Event created");
+
+        return event.getId();
+    }
+
+    public SportEventResponse getEvent(Long id) {
+        Optional<SportEvent> event = Optional.ofNullable(events.get(id));
+        return new SportEventResponse(id, event.orElseThrow().getName());
+    }
+
+    public SportEventResponse updateEvent(Long id, UpdateSportEventRequest eventRequest) {
+
+        Optional<SportEvent> event = Optional.ofNullable(events.get(id));
+        SportEvent sportEvent = event.orElseThrow();
+        sportEvent.setName(eventRequest.name());
+        log.info("Event updated");
+
+        return new SportEventResponse(id, sportEvent.getName());
+    }
+
+    public void deleteEvent(Long id) {
+        SportEvent event = Optional.ofNullable(events.get(id)).orElseThrow();
+        events.remove(event.getId());
+        log.info("Event removed");
     }
 }
