e74227cf4252bcaa138442ea274dce05dacba4f2
Jarosław Klimas <admin@javappa.com>
2023-04-09 19:05:41 +0200

    JVPA-22 Bean-Driven Configuration

---
+++ spring-boot-3-web/src/main/java/com/javappa/start/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.javappa.start.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi api() {
+        return GroupedOpenApi.builder()
+                .group("all")
+                .pathsToMatch("/api/sport-events/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi byIdApi() {
+        return GroupedOpenApi.builder()
+                .group("byId")
+                .pathsToMatch("/api/sport-events/{id}")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI openApi() {
+        return new OpenAPI()
+                .info(new Info().title("Sporting Events Web")
+                        .description("Sporting Events Web is a web application that allows users to browse and manage sports events")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
+
+}
